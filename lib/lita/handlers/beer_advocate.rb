0144fd6d5775d742264af6e0806fdbb9975383ea
Adam Zaninovich <adam.zaninovich@activenetwork.com>
2015-03-04 11:08:52 -0600

    adds beer_me route

--- lib/lita/handlers/beer_advocate.rb
+++ lib/lita/handlers/beer_advocate.rb
@@ -1,6 +1,11 @@
 module Lita
   module Handlers
     class BeerAdvocate < Handler
+      route /\bbeer me\b/, :beer_me, command: true
+
+      def beer_me response
+        response.reply 'this is a work in progress'
+      end
     end
 
     Lita.register_handler(BeerAdvocate)
